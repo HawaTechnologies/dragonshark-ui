08e82eafc412d7f4320606f01f8e601fd0f51fad
Luis Masuelli <luisfmasuelli@gmail.com>
2024-07-30 17:38:41 -0500

    Starting the Electron app.

---
+++ front-end/src/main.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('node:path');
+
+const createWindow = () => {
+    // Create
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'browser', 'preload.js')
+        }
+    })
+
+    win.loadFile('browser/index.html');
+}
+
+app.whenReady().then(() => {
+    // Windows / Linux experience: if all the
+    // windows are closed, then close the app.
+    // macOS: Do not close the app. It will
+    // become inactive.
+    app.on('window-all-closed', () => {
+        if (process.platform !== 'darwin') app.quit();
+    })
+
+    // Create a window on launching the app.
+    createWindow();
+
+    // Conditionally create a window on activating
+    // the app, if no window is open. This is for
+    // systems like macOS (which work like Android
+    // regarding experience: apps are not always
+    // closed in the same way as Windows / Linux).
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    });
+});
+
