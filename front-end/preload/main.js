61941d474317b4080b9d4d6df98199a22d3a8000
Luis Masuelli <luisfmasuelli@gmail.com>
2024-08-04 19:35:07 -0500

    Adding some commands.

--- front-end/preload/main.js
+++ front-end/preload/main.js
@@ -1,3 +1,13 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('myAPI', {
+    node: () => process.versions.node,
+    chrome: () => process.versions.chrome,
+    electron: () => process.versions.electron,
+    // someCommand: () => ipcRenderer.invoke("someCommand")
+    // // It can include arguments.
+});
+
 window.addEventListener('DOMContentLoaded', () => {
     const replaceText = (selector, text) => {
         const element = document.getElementById(selector)
