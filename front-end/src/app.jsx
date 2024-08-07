b8f236bfa19552c177842958a03097aba9893a87
Luis Masuelli <luisfmasuelli@gmail.com>
2024-08-06 19:37:13 -0500

    Adding the App.jsx component.

--- front-end/src/app.jsx
+++ front-end/src/app.jsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import './styles/index.css';
 import { createRoot } from 'react-dom/client';
+import App from "./components/App.jsx";
 
 const root = createRoot(document.getElementById("app"));
-root.render(<h2>Hello from React!</h2>);
+root.render(<App />);
