90a0bac3704faca54781619c8d53e71ad0773659
Luis Masuelli <luisfmasuelli@gmail.com>
2024-08-11 11:31:55 -0500

    Adding the listExternalDeviceDirs function.

--- front-end/src/main_utils/games.js
+++ front-end/src/main_utils/games.js
@@ -1,3 +1,20 @@
 const { exec } = require("./processes");
 
-module.exports = {}
+/**
+ * Lists the available external devices' directories.
+ * @returns {Promise<{dirs: (string[]), code: number}>} The list of directories (async function).
+ */
+async function listExternalDeviceDirs() {
+    // Run the process.
+    const {stdout, result} = await exec("dragonshark-games-enumerate-external-device-dirs");
+
+    // Get the code.
+    const code = result?.code || 0;
+
+    // Parse the results.
+    return {code, dirs: code ? [] : stdout.trim().split("\n")};
+}
+
+module.exports = {
+    listExternalDeviceDirs
+}
