1a7c6f982be042afac981e7e73c22767c2abc755
Luis Masuelli <luisfmasuelli@gmail.com>
2024-08-11 11:00:43 -0500

    Adding the listWLANInterfaces function.

--- front-end/src/main_utils/network.js
+++ front-end/src/main_utils/network.js
@@ -1,20 +1,38 @@
 const { exec } = require("./processes");
 
 /**
- * Lists the available interfaces.
+ * Lists the available IPv4 interfaces.
+ * @returns {Promise<{interfaces: (Array[]), code: any}>} The list of [address, type, wireless?] triples (async function).
  */
 async function listIPv4Interfaces() {
     // Run the process.
     const {stdout, stderr, result} = await exec("dragonshark-network-list-ipv4-interfaces");
 
     // Get the code.
-    const code = result?.code || null;
+    const code = result?.code || 0;
+
+    // Parse the results.
     return {code, interfaces: code ? [] : stdout.trim().split("\n").map(e => {
         const [address, type, wireless] = e.trim().split(",");
         return [address, type, wireless === "yes"];
     }).filter(e => e[1] !== "unknown")};
 }
 
+/**
+ * Lists the available wireless network interfaces.
+ * @returns {Promise<{interfaces: (string[]), code: any}>} The list of wireless network interface names.
+ */
+async function listWLANInterfaces() {
+    // Run the process.
+    const {stdout, stderr, result} = await exec("dragonshark-network-list-wlan-interfaces");
+
+    // Get the code.
+    const code = result?.code || 0;
+
+    // Parse the results.
+    return {code, interfaces: code ? [] : stdout.trim().split("\n")};
+}
+
 module.exports = {
-    listIPv4Interfaces
+    listIPv4Interfaces, listWLANInterfaces
 }
