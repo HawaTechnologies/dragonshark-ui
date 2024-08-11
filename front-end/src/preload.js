e6872b10cc472ff73524401b5a94d1f1d8adc33c
Luis Masuelli <luisfmasuelli@gmail.com>
2024-08-11 12:40:24 -0500

    Adding the IPC functions.

--- front-end/src/preload.js
+++ front-end/src/preload.js
@@ -1,2 +1,19 @@
-// See the Electron documentation for details on how to use preload scripts:
-// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+const { contextBridge, ipcRenderer } = require('electron')
+
+contextBridge.exposeInMainWorld('dragonsharkAPI', {
+    listIPv4Interfaces: () => ipcRenderer.invoke("listIPv4Interfaces"),
+    listWLANInterfaces: () => ipcRenderer.invoke("listWLANInterfaces"),
+    listWirelessNetworks: () => ipcRenderer.invoke("listWirelessNetworks"),
+    connectToNetwork: (ssid, password, interfaceName) => ipcRenderer.invoke(
+        "connectToNetwork", ssid, password, interfaceName
+    ),
+    disconnectFromNetwork: (interfaceName) => ipcRenderer.invoke(
+        "disconnectFromNetwork", interfaceName
+    ),
+    listExternalDeviceDirs: () => ipcRenderer.invoke("listExternalDeviceDirs"),
+    setRomsDir: (dir) => ipcRenderer.invoke("setRomsDir", dir),
+    getRomsDir: () => ipcRenderer.invoke("getRomsDir"),
+    setupSavesDirs: () => ipcRenderer.invoke("setupSavesDirs"),
+    backupSavesDirs: (dir) => ipcRenderer.invoke("backupSavesDirs", dir),
+    restoreSavesDirs: (dir) => ipcRenderer.invoke("restoreSavesDirs", dir),
+});
