94ab35be2cc56c7a781478b6a35b5690ff587d27
Luis Masuelli <luisfmasuelli@gmail.com>
2024-08-11 12:17:00 -0500

    Adding the saves' backup and restore functions.

--- front-end/src/main_utils/games.js
+++ front-end/src/main_utils/games.js
@@ -61,6 +61,41 @@ async function setupSavesDirs() {
     return {code, stdout, stderr};
 }
 
+/**
+ * Makes a backup of the current games' saves.
+ * @param dir The directory into which the generated backup.zip file will be stored.
+ * @returns {Promise<{code: number, stdout: string, stderr: string}>} The {code, stdout, stderr} with the result of the operation (async function).
+ */
+async function backupSavesDirs(dir) {
+    // Run the process.
+    const {stdout, stderr, result} = await exec(`dragonshark-games-saves-backup ${escapeShellArg(dir)}`);
+
+    // Get the code.
+    const code = result?.code || 0;
+
+    // Parse the results.
+    return {code, stdout, stderr};
+}
+
+/**
+ * Restores the backup.zip into /mnt/SAVES file from a directory. For this
+ * function to be safe, the $USER must belong to the owner group of that
+ * directory, and the directory must have g+rw permissions as well, but no
+ * other user should be directly allowed to edit (w) that directory.
+ * @param dir The directory from which the backup.zip file will be read.
+ * @returns {Promise<{code: number, stdout: string, stderr: string}>} The {code, stdout, stderr} with the result of the operation (async function).
+ */
+async function restoreSavesDirs(dir) {
+    // Run the process.
+    const {stdout, stderr, result} = await exec(`dragonshark-games-saves-restore ${escapeShellArg(dir)}`);
+
+    // Get the code.
+    const code = result?.code || 0;
+
+    // Parse the results.
+    return {code, stdout, stderr};
+}
+
 module.exports = {
-    listExternalDeviceDirs, setRomsDir, getRomsDir, setupSavesDirs
+    listExternalDeviceDirs, setRomsDir, getRomsDir, setupSavesDirs, backupSavesDirs, restoreSavesDirs
 }
