4ada48ea22620de6ae23e7425ad5e8da68b9e297
Luis Masuelli <luisfmasuelli@gmail.com>
2024-08-13 13:42:33 -0500

    Reducing the logo.

--- front-end/src/components/common/Logo.jsx
+++ front-end/src/components/common/Logo.jsx
@@ -4,6 +4,6 @@ import full from "../../images/full.png";
 export default function Logo() {
     return <img src={full} alt="Logo" style={{
         position: "absolute", left: "50%", top: "0",
-        transform: "translate(-50%, 0)"
+        transform: "translate(-50%, -25%) scale(0.5, 0.5)"
     }} />
 }
