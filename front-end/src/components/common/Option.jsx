9a06ee87a4ec14c6d996d1aea8f9b8341a17b2fb
Luis Masuelli <luisfmasuelli@gmail.com>
2024-08-25 21:02:27 -0500

    From the option, remove the button reference.

--- front-end/src/components/common/Option.jsx
+++ front-end/src/components/common/Option.jsx
@@ -1,21 +1,17 @@
 import * as React from 'react';
 
 /**
- * This is a menu option. These typically appear in
- * a group of 4 options and are triggered by the BUp/1,
- * BRight/2, BDown/3, BLeft/4 buttons.
+ * This is a menu option. It has an image and a caption.
  * @param style The extra style to the wrapper.
  * @param className The extra class name to the wrapper.
  * @param image The image to show.
- * @param button The button to associate. R2 cannot be
- * used for this purpose.
  * @param caption The caption to use.
  * @constructor
  */
-export default function Option({ style, className, image, button, caption }) {
+export default function Option({ style, className, image, caption }) {
     style ||= {};
     return <div className={className} style={{...style, textAlign: 'center'}}>
         <img className="option" src={image} alt={caption} />
-        <div className="text-soft">{button} {caption}</div>
+        <div className="text-soft">{caption}</div>
     </div>
 }
