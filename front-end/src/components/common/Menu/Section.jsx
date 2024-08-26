876ea5091c9864d4f62e3097802bf62f40a4e2d3
Luis Masuelli <luisfmasuelli@gmail.com>
2024-08-25 21:52:20 -0500

    Adding the Section component.

---
+++ front-end/src/components/common/Menu/Section.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+/**
+ * This is a menu section. It only conditionally shows a section's elements.
+ * @param children The children to render.
+ * @param visible Whether to render the section or not.
+ * @constructor
+ */
+export default function Section({ children, visible }) {
+    if (!visible) return null;
+    return <>{children}</>;
+}
