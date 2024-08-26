43c26d137ad50549ae66f17a4b06e32fb108e2b6
Luis Masuelli <luisfmasuelli@gmail.com>
2024-08-25 21:25:29 -0500

    Adding the `selected` prop to the `Option` component.

--- front-end/src/components/common/Option.jsx
+++ front-end/src/components/common/Option.jsx
@@ -6,12 +6,13 @@ import * as React from 'react';
  * @param className The extra class name to the wrapper.
  * @param image The image to show.
  * @param caption The caption to use.
+ * @param selected Whether the current option is selected.
  * @constructor
  */
-export default function Option({ style, className, image, caption }) {
+export default function Option({ style, className, image, caption, selected }) {
     style ||= {};
     return <div className={className} style={{...style, textAlign: 'center'}}>
-        <img className="option" src={image} alt={caption} />
+        <img className={"option " + (selected ? "glow" : "")} src={image} alt={caption} />
         <div className="text-soft">{caption}</div>
     </div>
 }
