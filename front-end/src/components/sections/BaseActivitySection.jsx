2dea72c4264e7ec1f5624261698c3dfefc5563ab
Luis Masuelli <luisfmasuelli@gmail.com>
2024-08-29 17:42:38 -0500

    Adding more features to the base activity (e.

--- front-end/src/components/sections/BaseActivitySection.jsx
+++ front-end/src/components/sections/BaseActivitySection.jsx
@@ -2,23 +2,37 @@ import * as React from 'react';
 import { useNavigate } from "react-router-dom";
 // Images
 import {useGamepad, usePressEffect} from "../hooks/gamepad";
-import {useRef} from "react";
+import {useEffect, useRef, useState} from "react";
 import Panel from "../common/Panel.jsx";
 import {L1, L2} from "../common/icons/TextButton";
 
 /**
  * The base activity section component.
+ * @param caption The caption of the section.
+ * @param children The children.
+ * @param backPath The path to navigate back to.
+ * @param backTimeout The timeout until the back button is active.
  * @constructor
  */
-export default function BaseActivitySection({ caption, children }) {
+export default function BaseActivitySection({ caption, children, backPath, backTimeout = 1000 }) {
     const navigate = useNavigate();
-    const { LT } = useGamepad();
+    const [backReady, setBackReady] = useState(false);
+    const { LT, LB } = useGamepad();
     const ref = useRef();
+    const refBack = useRef();
+
     ref.current = () => {
         navigate("/");
     }
+    refBack.current = () => {
+        if (backReady && backPath) navigate(backPath);
+    }
 
     usePressEffect(LT, 500, ref);
+    usePressEffect(LB, backTimeout, refBack);
+    useEffect(() => {
+        setTimeout(() => setBackReady(true), backTimeout);
+    }, []);
 
     return <Panel style={{position: "absolute", left: "48px", right: "48px", bottom: "208px", top: "288px"}}>
         <div className="text-red" style={{position: "absolute", left: "48px", right: "48px"}}>Press <L1/> to leave</div>
