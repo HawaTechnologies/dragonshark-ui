78074ba55596ad8d1817492bd94da3d9675ed23f
Luis Masuelli <luisfmasuelli@gmail.com>
2024-08-30 18:32:30 -0500

    Removing a redundant property.

--- front-end/src/components/sections/BaseActivitySection.jsx
+++ front-end/src/components/sections/BaseActivitySection.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import {useGamepad, usePressEffect} from "../hooks/gamepad";
 import {useEffect, useRef, useState} from "react";
 import Panel from "../common/Panel.jsx";
-import {L1, L2} from "../common/icons/TextButton.jsx";
+import {L1} from "../common/icons/TextButton.jsx";
 
 /**
  * The base activity section component.
@@ -35,7 +35,7 @@ export default function BaseActivitySection({ caption, children, backPath, backT
     }, []);
 
     return <Panel style={{position: "absolute", left: "48px", right: "48px", bottom: "208px", top: "288px"}}>
-        <div className="text-red" style={{position: "absolute", left: "48px", right: "48px"}}>Press <L1/> to leave</div>
+        <div className="text-red" style={{position: "absolute", left: "48px"}}>Press <L1/> to leave</div>
         <div className="text-soft" style={{position: "absolute", top: "48px", left: "50%", transform: "translateX(-50%)", fontSize: "20px"}}>{caption}</div>
         {children}
     </Panel>;
