9029036640d35faadbfc6b0dbb7ab0a94c561305
Luis Masuelli <luisfmasuelli@gmail.com>
2024-09-05 21:38:12 -0500

    Also returning stderr from network commands.

--- front-end/src/main_utils/network.js
+++ front-end/src/main_utils/network.js
@@ -2,11 +2,11 @@ const { exec, escapeShellArg } = require("./processes");
 
 /**
  * Lists the available IPv4 interfaces.
- * @returns {Promise<{interfaces: (Array[]), code: number}>} The list of [address, type, wireless?] triples (async function).
+ * @returns {Promise<{interfaces: (Array[]), code: number, stderr: string}>} The list of [address, type, wireless?] triples (async function).
  */
 async function listIPv4Interfaces() {
     // Run the process.
-    const {stdout, result} = await exec("dragonshark-network-list-ipv4-interfaces");
+    const {stdout, result, stderr} = await exec("dragonshark-network-list-ipv4-interfaces");
 
     // Get the code.
     const code = result?.code || 0;
@@ -15,31 +15,31 @@ async function listIPv4Interfaces() {
     return {code, interfaces: code ? [] : stdout.trim().split("\n").map(e => {
         const [address, type, wireless] = e.trim().split(",");
         return [address, type, wireless === "yes"];
-    }).filter(e => e[1] !== "unknown")};
+    }).filter(e => e[1] !== "unknown"), stderr};
 }
 
 /**
  * Lists the available wireless network interfaces.
- * @returns {Promise<{interfaces: (string[]), code: number}>} The list of wireless network interface names.
+ * @returns {Promise<{interfaces: (string[]), code: number, stderr: string}>} The list of wireless network interface names.
  */
 async function listWLANInterfaces() {
     // Run the process.
-    const {stdout, result} = await exec("dragonshark-network-list-wlan-interfaces");
+    const {stdout, result, stderr} = await exec("dragonshark-network-list-wlan-interfaces");
 
     // Get the code.
     const code = result?.code || 0;
 
     // Parse the results.
-    return {code, interfaces: code ? [] : stdout.trim().split("\n")};
+    return {code, interfaces: code ? [] : stdout.trim().split("\n"), stderr};
 }
 
 /**
  * Lists the available wireless networks.
- * @returns {Promise<{interfaces: (Array[]), code: number}>} The list of [active, ssid, signal] triples (async function).
+ * @returns {Promise<{interfaces: (Array[]), code: number, stderr: string}>} The list of [active, ssid, signal] triples (async function).
  */
 async function listWirelessNetworks() {
     // Run the process.
-    const {stdout, result} = await exec("dragonshark-network-list-wireless-networks");
+    const {stdout, result, stderr} = await exec("dragonshark-network-list-wireless-networks");
 
     // Get the code.
     const code = result?.code || 0;
@@ -51,7 +51,7 @@ async function listWirelessNetworks() {
             active === "*", ssid, parseInt(signal),
             (security === "" || security === "--") ? null : security.trim().split(/\s+/)
         ];
-    }).filter(e => e[1])};
+    }).filter(e => e[1]), stderr};
 }
 
 /**
