b5175daf8aa4caa4e582ccac1f3c8d039376ba91
Luis Masuelli <luisfmasuelli@gmail.com>
2025-01-05 15:05:54 -0500

    Implementing the sound commands.

--- front-end/src/main_utils/sound.js
+++ front-end/src/main_utils/sound.js
@@ -1,9 +1,26 @@
+const {exec, escapeShellArg} = require("./processes");
+
 /**
  * Gets the master volume.
  * @returns {Promise<void>} The master volume (async function).
  */
 async function getVolume() {
+    // Run the process.
+    const {stdout, stderr, result} = await exec(`dragonshark-sound-get-volume`);
+    const output = (stdout || "").trim();
 
+    // Get the code.
+    const code = result?.code || 0;
+
+    if (code) {
+        return {
+            code, status: "error", hint: "unknown", dump: stderr
+        }
+    } else {
+        return {
+            code: 0, volume: parseInt(output.trim())
+        }
+    }
 }
 
 /**
@@ -12,7 +29,21 @@ async function getVolume() {
  * @returns {Promise<void>} Nothing (async function).
  */
 async function setVolume(volume) {
+    const {stdout, stderr, result} = await exec(`dragonshark-sound-set-volume ${escapeShellArg(volume.toString())}`);
+    const output = (stdout || "").trim();
 
+    // Get the code.
+    const code = result?.code || 0;
+
+    if (code) {
+        return {
+            code, status: "error", hint: "unknown", dump: stderr
+        }
+    } else {
+        return {
+            code: 0
+        }
+    }
 }
 
 module.exports = {
