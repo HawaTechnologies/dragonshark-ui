4bdef602a4d1769ddb98af72fd2eb05fbb45898c
Luis Masuelli <luisfmasuelli@gmail.com>
2025-01-05 21:27:52 -0500

    Adding more logic to the date/time component.

--- front-end/src/components/sections/userexperience/DateTime.jsx
+++ front-end/src/components/sections/userexperience/DateTime.jsx
@@ -1,19 +1,20 @@
 import * as React from 'react';
 import BaseActivitySection from "../BaseActivitySection.jsx";
 import {useCallback, useEffect, useMemo, useRef, useState} from "react";
-import {getDiscreteAxisStates, useGamepad} from "../../hooks/gamepad";
+import {getDiscreteAxisStates, useGamepad, usePressEffect} from "../../hooks/gamepad.js";
 
 const datetime = window.dragonSharkAPI.datetime;
 
 /**
  * Updates the relevant properties of this date/time interface.
  * @param timezone The timezone.
+ * @param canNTP Whether it can do NTP or not.
  * @param ntp Whether to use NTP or not.
  * @returns {Promise<void>} Nothing (async function).
  */
-async function updateAll({timezone, ntp}) {
-    await datetime.setTimezone(timezone);
-    await datetime.setNTPActive(ntp);
+async function updateAll({timezone, canNTP, ntp}) {
+    if (timezone) await datetime.setTimezone(timezone);
+    if (canNTP) await datetime.setNTPActive(ntp);
 }
 
 /**
@@ -37,17 +38,17 @@ export default function DateTime() {
     const [timezones, setTimezones] = useState([]);
     const [timeData, setTimeData] = useState(null);
     // Fake hook for the timezone.
-    const timezone = timeData.timezone;
-    const setTimezone = useCallback(function() {
+    const timezone = timeData?.timezone;
+    const setTimezone = useCallback(function(timezone) {
         setTimeData({...timeData, timezone});
     }, [timeData, setTimeData]);
     // Fake hook for the NTP property.
-    const ntp = timeData.ntp;
-    const setNTP = useCallback(function() {
-        setTimeData({...timeData, ntp});
+    const ntp = timeData?.ntp;
+    const toggleNTP = useCallback(function() {
+        setTimeData({...timeData, ntp: !ntp});
     }, [timeData, setTimeData]);
     // Fake read-only properties.
-    const { time, canNTP, ntpSynchronized } = timeData;
+    const { time, canNTP, ntpSynchronized } = timeData || {};
 
     // A small function I'll use always as a regular function.
     // Why? Because the array of timezones will always change
@@ -70,6 +71,42 @@ export default function DateTime() {
         const _ = refreshAll();
         return () => clearInterval(interval);
     }, []);
+
+    refLeft.current = function () {
+        const index = timezones.indexOf(timezone);
+        if (index < 0) {
+            setTimezone(timezones[0]);
+        } else if (index === 0) {
+            setTimezone(timezones[timezones.length - 1]);
+        } else {
+            setTimezone(timezones[index - 1]);
+        }
+    }
+    refRight.current = function () {
+        const index = timezones.indexOf(timezone);
+        if (index < 0) {
+            setTimezone(timezones[0]);
+        } else if (index === timezones.length - 1) {
+            setTimezone(timezones[0]);
+        } else {
+            setTimezone(timezones[index - 1]);
+        }
+    }
+    refNTP.current = function () {
+        toggleNTP();
+    }
+    refRefreshAll.current = function () {
+        const _ = refreshAll();
+    }
+    refUpdateAll.current = function () {
+        const _ = updateAll({timezone, canNTP, ntp});
+    }
+
+    usePressEffect(timezones.length !== 0 && timeData && leftPressed, 500, refLeft);
+    usePressEffect(timezones.length !== 0 && timeData && rightPressed, 500, refRight);
+    usePressEffect(timeData && buttonA, 500, refUpdateAll);
+    usePressEffect(timeData && buttonB, 500, refRefreshAll);
+    usePressEffect(timeData && buttonX, 500, refNTP);
 
     return <BaseActivitySection caption="Date & Time" backPath="/user-experience">
         <div style={{position: "absolute", left: "50%", top: "50%", transform: "translate(-50%, -50%)"}}>
