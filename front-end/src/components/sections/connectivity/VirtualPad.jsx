73b5090a98934283622d19b795d8c67512d28063
Luis Masuelli <luisfmasuelli@gmail.com>
2025-01-07 17:44:17 -0500

    Adding part of the implementation.

--- front-end/src/components/sections/connectivity/VirtualPad.jsx
+++ front-end/src/components/sections/connectivity/VirtualPad.jsx
@@ -3,6 +3,7 @@ import BaseActivitySection from "../BaseActivitySection.jsx";
 import {getDiscreteAxisStates, useGamepad, usePressEffect} from "../../hooks/gamepad.js";
 import {useEffect, useRef, useState} from "react";
 import {getVirtualPadServerStatus} from "../../utils/virtualpad.js";
+import {BUp} from "../../common/icons/RightPanelButton.jsx";
 
 const virtualpad = window.dragonSharkAPI.virtualpad;
 
@@ -28,24 +29,29 @@ export default function VirtualPad() {
     const [status, setStatus] = useState(null);
     const [statusRefreshKey, setStatusRefreshKey] = useState(0);
     useEffect(() => {
-        const interval = setInterval(() => {
-
-        }, 4000);
+        async function getAndUpdateVirtualPadServerStatus() {
+            const result = await getVirtualPadServerStatus();
+            setStatus(result);
+        }
+        const interval = setInterval(getAndUpdateVirtualPadServerStatus, 4000);
+        const _ = getAndUpdateVirtualPadServerStatus();
         return () => clearInterval(interval);
     }, [statusRefreshKey]);
     // 2. The selected pad index, object, password.
+    // TODO There's an error somewhere here since pressing LEFT twice triggers
+    // TODO access error of the property '0'. Debug this later.
     const [selectedPadIndex, setSelectedPadIndex] = useState(0);
-    const selectedPad = status ? status.pads[selectedPadIndex] : null;
+    const selectedPad = status?.pads ? status.pads[selectedPadIndex] : null;
     const [padStatus, user] = selectedPad || [null, null];
     const password = status ? status.passwords[selectedPadIndex] : null;
     // Handlers:
     refLeft.current = function() {
         if (!(status?.pads)) return;
-        setStatus((selectedPadIndex === 0) ? status.pads.length - 1 : selectedPadIndex - 1);
+        setStatus((selectedPadIndex === 0) ? (status.pads.length - 1) : (selectedPadIndex - 1));
     }
     refRight.current = function() {
         if (!(status?.pads)) return;
-        setStatus((selectedPadIndex === status.pads.length) ? 0 : selectedPadIndex + 1);
+        setStatus((selectedPadIndex === status.pads.length) ? 0 : (selectedPadIndex + 1));
     }
     refResetPassword.current = async function() {
         if (!(status?.pads)) return;
@@ -66,7 +72,7 @@ export default function VirtualPad() {
     refServer.current = async function() {
         if (!status) return;
         // Start/Stop the server.
-        if (status.connected) {
+        if (!status.connected) {
             await virtualpad.startServer();
         } else {
             await virtualpad.stopServer();
@@ -82,8 +88,48 @@ export default function VirtualPad() {
     usePressEffect(buttonY, 500, refServer);
 
     return <BaseActivitySection caption="Virtual Pad" backPath="/connectivity">
-        <div style={{position: "absolute", left: "50%", top: "50%", transform: "translate(-50%, -50%)"}}>
-            This section is not ready yet.
+        <div style={{position: "absolute", left: "50%", top: "50%", width: "75%", height: "75%",
+                     transform: "translate(-50%, -50%)"}}>
+            <p style={{fontSize: "14px"}}>
+                This section is for VirtualPad configuration. VirtualPad, by HawaTechnologies, is a mobile
+                client application that allows players to turn their mobile devices into a standard joystick
+                with D-Pad, L1/L2/R1/R2/Select/Start buttons, A/B/X/Y buttons and analog axes. While the
+                experience is not the same as a regular joystick, it is expected for it to be useful in
+                simple games and a substitute of actual, physical, joysticks.
+            </p>
+            <p style={{fontSize: "14px"}}>
+                VirtualPad comes built-in and supports up to 8 players (this means: up to 8 players can
+                connect to the same VirtualPad server). This has nothing to do with any physical joystick
+                that might be connected to this console (directly or via Blue Tooth), which means that the
+                amount of total joysticks (adding up physical and VirtualPad ones) connected to this console
+                might be even greater than 8. Please consider, however, that most games support only up to 8
+                joysticks and, actually, many of them even support less than 4. Also, it is important to know
+                that it should not be expected the numeric order to be preserved (especially if the VirtualPad
+                service is disabled by the user) among virtual and/or physical joysticks.
+            </p>
+            {!status && <p style={{textAlign: "center"}}>
+                Fetching VirtualPad status...
+            </p>}
+            {status && status.result !== "success" && <p style={{textAlign: "center"}}>
+                There was an error while trying to recover the VirtualPad status.
+            </p>}
+            {status && status.result === "success" && status.connected && <p>
+                VirtualPad server is running.<br />
+                Press <BUp /> to stop VirtualPad server.
+            </p>}
+            {status && status.result === "success" && !status.connected && <p>
+                VirtualPad server is not running.<br />
+                Press <BUp /> to start VirtualPad server.
+            </p>}
+            {status && status.result === "success" && status.connected && <div>
+                <div style={{marginBottom: "40px"}}>
+                    {/* Here: Horizontal list of 8 pads being selectable (0 to 7) */}
+                </div>
+                {padStatus !== null && <div>
+                    {/* Here: Display of the current pad in terms of: padStatus, user, password */}
+                    {/* Here: Also, prompts for actions: kick user, refresh password */}
+                </div>}
+            </div>}
         </div>
     </BaseActivitySection>;
 }
