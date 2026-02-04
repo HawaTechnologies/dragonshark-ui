79c61e34093e8a18b0096aacfcb84ae1e3e4e7be
Luis Masuelli <luisfmasuelli@gmail.com>
2026-02-03 22:00:16 -0500

    Adding the save files interface.

--- front-end/src/components/sections/play/SaveFiles.jsx
+++ front-end/src/components/sections/play/SaveFiles.jsx
@@ -1,14 +1,97 @@
 import * as React from 'react';
 import BaseActivitySection from "../BaseActivitySection.jsx";
+import {useState, useEffect} from "react";
+import {BDown, BLeft, BRight} from "../../common/icons/RightPanelButton.jsx";
+import Select from "../../common/Select.jsx";
+import {useGamepad, usePressEffect} from "../../hooks/gamepad";
+
+const games = window.dragonSharkAPI.games;
 
 /**
  * The Play > Save Files section.
  * @constructor
  */
 export default function SaveFiles() {
+    const [externalDevices, setExternalDevices] = useState([]);
+    const [backupDevice, setBackupDevice] = useState(null);
+    const [currentTimeout, setCurrentTimeout] = useState(0);
+    const [message, setMessage_] = useState(null);
+    const setMessage = function(message) {
+        setMessage_(message);
+        if (currentTimeout) {
+            clearTimeout(currentTimeout);
+        }
+        setCurrentTimeout(0);
+    }
+
+    function refreshExternalDevices() {
+        setMessage("Refreshing storage devices...");
+        return games.listExternalDeviceDirs().then(({ dirs }) => {
+            setExternalDevices(dirs);
+            setMessage("");
+        }).catch((reason) => {
+            console.error("Error on devices refresh:", reason);
+            setMessage("Error refreshing storage devices.");
+            setTimeout(() => {
+                setMessage("");
+            }, 2000);
+        });
+    }
+
+    const {
+        buttonA: keyAPressed, buttonB: keyBPressed, buttonX: keyXPressed
+    } = useGamepad();
+    usePressEffect(keyAPressed, 500, () => {
+        setMessage("Making backup.zip file...");
+        games.backupSavesDirs(backupDevice).catch(() => {
+            setMessage("Error creating backup.zip file.");
+        }).then(() => {
+            setMessage("Backup created successfully.");
+            setTimeout(() => {
+                setMessage("");
+            }, 2000);
+        });
+    }, 1000);
+    usePressEffect(keyBPressed, 500, () => {
+        setMessage("Restoring backup.zip file...");
+        games.restoreSavesDirs(backupDevice).catch(() => {
+            setMessage("Error restoring backup.zip file (perhaps does not exist).");
+        }).then(() => {
+            setMessage("Restore performed successfully.");
+            setTimeout(() => {
+                setMessage("");
+            }, 2000);
+        });
+    }, 1000);
+    usePressEffect(keyXPressed, 500, () => {
+        refreshExternalDevices();
+    }, 1000);
+    useEffect(() => {
+        refreshExternalDevices();
+    }, []);
+
     return <BaseActivitySection caption="Save Files" backPath="/play">
-        <div style={{position: "absolute", left: "50%", top: "50%", transform: "translate(-50%, -50%)"}}>
-            This section is not ready yet.
+        {/*
+          * Managing the saves file involves:
+          * - Ensuring the saves layout is present - games.setupSavesDirs().
+          * - Refreshing the list of available units - games.listExternalDeviceDirs().
+          * - Backup to a certain directory - games.backupSavesDirs(dir).
+          * - Restore from a certain directory - games.restoreSavesDirs(dir).
+          * The directory in the parameters is a directory among the ones
+          * returned by games.listExternalDeviceDirs().
+          */}
+        <div className="text-bigger" style={{
+            position: "absolute",
+            left: "50%", top: "50%", width: "80%",
+            transform: "translate(-50%, -50%)"
+        }}>
+            <div>
+                Storage unit: <Select value={backupDevice} onChange={setBackupDevice} options={externalDevices} />
+            </div>
+            <div>Press <BDown /> to do a saves backup to the storage unit.</div>
+            <div>Press <BRight /> to do a saves restore from the storage unit.</div>
+            <div>Press <BLeft /> to refresh the list of storage networks.</div>
+            {message && <div>{message}</div>}
         </div>
     </BaseActivitySection>;
 }
