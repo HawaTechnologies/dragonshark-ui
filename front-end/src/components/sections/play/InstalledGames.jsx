58069536b0b9776a168de447834a6f8ae5f9a557
Luis Masuelli <luisfmasuelli@gmail.com>
2026-02-07 12:08:54 -0500

    Starting the games section.

--- front-end/src/components/sections/play/InstalledGames.jsx
+++ front-end/src/components/sections/play/InstalledGames.jsx
@@ -1,14 +1,66 @@
 import * as React from 'react';
+import {useState, useEffect, useMemo} from "react";
+import {getDiscreteAxisStates, useGamepad, usePressEffect} from "../../hooks/gamepad.js";
 import BaseActivitySection from "../BaseActivitySection.jsx";
+
+const games = window.dragonSharkAPI.games;
+
+/**
+ * A preview of a single game data.
+ * @param currentGame The data of the current game.
+ * @constructor
+ */
+function GamePreview({ currentGame }) {
+    return <></>;
+}
+
+const TAIL_SIZE = 10;
+
+/**
+ * The list of games.
+ * @param value The current game index.
+ * @param onChange A setter for the current game index.
+ * @param options The list of options.
+ * @constructor
+ */
+function GamesList({
+    value, onChange, options
+}) {
+    return <></>;
+}
 
 /**
  * The Play > Installed Games section.
  * @constructor
  */
 export default function InstalledGames() {
+    // Defining state for the current list of games
+    // and the currently focused game.
+    const [gamesList, setGamesList] = useState([]);
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const currentGame = gamesList[currentIndex];
+    const {
+        buttonA: keyAPressed, buttonX: keyXPressed,
+    } = useGamepad();
+    useEffect(() => {
+        (async () => {
+            const games_ = await games.enumerateGames();
+            console.log(gamesList);
+            setGamesList(games_);
+            setCurrentIndex(Math.min(currentIndex, games_.length));
+        })();
+    }, []);
+    usePressEffect(keyXPressed, 500, async function () {
+        const games_ = await games.enumerateGames();
+        setGamesList(games_);
+        setCurrentIndex(Math.min(currentIndex, games_.length));
+    }, 1000);
+
     return <BaseActivitySection caption="Installed Games" backPath="/play">
-        <div style={{position: "absolute", left: "50%", top: "50%", transform: "translate(-50%, -50%)"}}>
-            This section is not ready yet.
+        <div style={{position: "absolute", left: "5%", top: "5%", right: "5%", bottom: "5%",
+                     display: "flex", flexDirection: "row"}}>
+            <GamePreview currentGame={currentGame} />
+            <GamesList value={currentIndex} onChange={setCurrentIndex} options={gamesList} />
         </div>
     </BaseActivitySection>;
 }
