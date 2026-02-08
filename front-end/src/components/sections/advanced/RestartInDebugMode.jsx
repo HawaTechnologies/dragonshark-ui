2151268795719b58217e1f3a1fd155463845173e
Luis Masuelli <luisfmasuelli@gmail.com>
2026-02-08 18:31:48 -0500

    Adding the restart-in-debug-mode stuff

--- front-end/src/components/sections/advanced/RestartInDebugMode.jsx
+++ front-end/src/components/sections/advanced/RestartInDebugMode.jsx
@@ -1,14 +1,26 @@
 import * as React from 'react';
 import BaseActivitySection from "../BaseActivitySection.jsx";
+import {useGamepad, usePressEffect} from "../../hooks/gamepad.js";
+import {BLeft, BRight} from "../../common/icons/RightPanelButton.jsx";
+
+
+const system = window.dragonSharkAPI.system;
+
 
 /**
  * The Advanced > Restart in Debug Mode section.
  * @constructor
  */
 export default function RestartInDebugMode() {
+    // First, get joystick status. Then, capture the buttons.
+    const {buttonX, buttonB} = useGamepad();
+    usePressEffect(buttonX && buttonB, 500, function() {
+        system.restartInDebugMode();
+    });
+
     return <BaseActivitySection caption="Restart in Debug Mode" backPath="/advanced">
         <div style={{position: "absolute", left: "50%", top: "50%", transform: "translate(-50%, -50%)"}}>
-            This section is not ready yet.
+            Hold <BLeft /> + <BRight /> to restart in debug mode.
         </div>
     </BaseActivitySection>;
 }
