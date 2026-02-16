0d617ef64693d4780fd2a0ce68caded61ce1f08f
Luis Masuelli <luisfmasuelli@gmail.com>
2026-02-16 14:56:58 -0500

    Adding the implementations of pairDevice and unpairDevice.

--- front-end/src/main_utils/bluetooth.js
+++ front-end/src/main_utils/bluetooth.js
@@ -5,7 +5,7 @@ const {exec, escapeShellArg} = require("./processes");
  * devices that are currently paired. These devices are
  * typically trusted as well (if untrusted, they can be
  * made trusted again by unpairing and re-pairing).
- * @returns {Promise<{code: number, data: null | {mac: string, name: string}[]}>} The list of paired devices.
+ * @returns {Promise<{code: number, data: null | {mac: string, name: string}[]}>} The list of paired devices (async function).
  */
 async function listPairedDevices() {
     // Run the process.
@@ -28,7 +28,7 @@ async function listPairedDevices() {
  * devices that are currently unpaired. Scanning for these
  * devices is time-consuming (the time is passed as argument).
  * @param time The time, in seconds. An integer value.
- * @returns {Promise<{code: number, data: null | {mac: string, name: string}[]}>} The list of unpaired devices.
+ * @returns {Promise<{code: number, data: null | {mac: string, name: string}[]}>} The list of unpaired devices (async function).
  */
 async function listUnpairedDevices(time) {
     // Run the process.
@@ -52,15 +52,37 @@ async function listUnpairedDevices(time) {
  * or a mac. In the case of a mac, it must be valid. In the
  * case of a device name, exactly an unpaired device entry
  * must exist for that device name.
- * @param device
- * @returns {Promise<void>}
+ * @param device The mac / name of the device to pair.
+ * @returns {Promise<{code: number, stdout: string, stderr: string}>} The {code, stdout, stderr} with the result of the operation (async function).
  */
 async function pairDevice(device) {
-    // dragonshark-bluetooth-pair-device <device>
+    // Run the process.
+    const {stdout, result, stderr} = await exec(`dragonshark-bluetooth-pair-device ${escapeShellArg(device)}`);
+
+    // Get the code.
+    const code = result?.code || 0;
+
+    // Forward the results directly.
+    return {code, stdout, stderr};
 }
 
+/**
+ * Unpairs a device. The chosen device can be a device name
+ * or a mac. In the case of a mac, it must be valid. In the
+ * case of a device name, exactly a paired device entry
+ * must exist for that device name.
+ * @param device The mac / name of the device to unpair.
+ * @returns {Promise<{code: number, stdout: string, stderr: string}>} The {code, stdout, stderr} with the result of the operation (async function).
+ */
 async function unpairDevice(device) {
-    // dragonshark-bluetooth-unpair-device <device>
+    // Run the process.
+    const {stdout, result, stderr} = await exec(`dragonshark-bluetooth-unpair-device ${escapeShellArg(device)}`);
+
+    // Get the code.
+    const code = result?.code || 0;
+
+    // Forward the results directly.
+    return {code, stdout, stderr};
 }
 
 module.exports = {
