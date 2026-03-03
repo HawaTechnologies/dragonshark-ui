d4a291f3f56f3222c8d6a440e07624693b6c81d3
Luis Masuelli <luisfmasuelli@gmail.com>
2026-03-03 10:02:15 -0500

    Ensuring the base activity buttons are only accounted if backPath is set.

--- front-end/src/components/sections/BaseActivitySection.jsx
+++ front-end/src/components/sections/BaseActivitySection.jsx
@@ -19,14 +19,14 @@ export default function BaseActivitySection({ caption, children, backPath, backT
     const { LT, LB } = useGamepad();
 
     usePressEffect(LT, 500, () => {
-        navigate("/");
+        if (backPath) navigate("/");
     });
     usePressEffect(LB, backTimeout, () => {
         if (backPath) navigate(backPath);
     }, null, 1000);
 
     return <Panel style={{position: "absolute", left: "48px", right: "48px", bottom: "208px", top: "288px"}}>
-        <div className="text-red" style={{position: "absolute", left: "48px"}}>Press <L1/> to leave</div>
+        {backPath && <div className="text-red" style={{position: "absolute", left: "48px"}}>Press <L1/> to leave</div>}
         <div className="text-soft text-bigger" style={{position: "absolute", top: "48px", left: "50%", transform: "translateX(-50%)"}}>{caption}</div>
         <div className="text-soft text-bigger" style={{position: "absolute", left: 0, top: 0, right: 0, bottom: 0}}>
             {children}
