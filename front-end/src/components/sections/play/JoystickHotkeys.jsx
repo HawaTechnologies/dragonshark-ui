163d8114b3183080993f208f01782b87ef0e946b
Luis Masuelli <luisfmasuelli@gmail.com>
2026-03-03 09:55:54 -0500

    Adding the joystick API reference to the JoystickHotkeys.jsx file.

--- front-end/src/components/sections/play/JoystickHotkeys.jsx
+++ front-end/src/components/sections/play/JoystickHotkeys.jsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import BaseActivitySection from "../BaseActivitySection.jsx";
 
+const joystick = window.dragonSharkAPI.joystick;
+
 /**
  * The Play > Joystick Hotkeys section.
  * @constructor
